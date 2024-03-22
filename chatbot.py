185ccc4750ddb82ff8c90fa589b08334fd9074ee
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-03-22 06:21:21 +0000

    Testing

--- chatbot.py
+++ chatbot.py
@@ -42,7 +42,7 @@ def get_recipe_name(ingredients):
     recipe_name = get_gemini_response(prompt)
     return recipe_name
 
-'''
+
 def get_recipe_steps(recipe_name):
     prompt = f""" You are a chef who can improvise a recipe even when not all the ingredients needed are there. 
     Can you generate numbered recipe steps for the recipe {recipe_name}?"""
@@ -53,16 +53,16 @@ def format_output(recipe_name, recipe_steps):
     output = f"Based on the ingredients you have, I have generated a recipe for you. The name of the recipe is {recipe_name}\n"
     output += f"Here are the steps to make the recipe:\n{recipe_steps}"
     return output
-'''
+
 if submit and user_input:
     st.session_state['chat_history'].append(("You", user_input))
     st.subheader("Chef Baloney's Response")
     recipe_name = get_recipe_name(user_input)
-    '''recipe_steps = get_recipe_steps(recipe_name)
+    recipe_steps = get_recipe_steps(recipe_name)
     output = format_output(recipe_name, recipe_steps)
     st.write(output)
     st.session_state['chat_history'].append(("Chef Baloney", output))
-'''
+
 footer_placeholder = st.empty()
 
 st.markdown("<h1 style='font-size: 17px; text-align: center; color:  #cb202d;'> Pro Tip: In case you're too lazy to cook, order good food from  <a href='https://play.google.com/store/apps/details?id=com.application.zomato&hl=en_IN&gl=US' target='_blank'>Zomato</a>. </h1>  <h2 style='font-size: 25px; text-align: center; color:  #cb202d;'> But don't you dare order anything from Swiggy!!!! </h2> <h3 style='font-size: 17px; text-align: center; color:  #cb202d;'> They made @thehappybaloney sad by not commenting under their reel. </h3>", unsafe_allow_html=True)
