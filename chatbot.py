2e7513323a559a1a56602f1edc14ecdbf04a542b
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-03-22 06:19:12 +0000

    Fix formatting issue in chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -60,9 +60,9 @@ if submit and user_input:
     recipe_name = get_recipe_name(user_input)
     '''recipe_steps = get_recipe_steps(recipe_name)
     output = format_output(recipe_name, recipe_steps)
-    st.write(output)'''
+    st.write(output)
     st.session_state['chat_history'].append(("Chef Baloney", output))
-
+'''
 footer_placeholder = st.empty()
 
 st.markdown("<h1 style='font-size: 17px; text-align: center; color:  #cb202d;'> Pro Tip: In case you're too lazy to cook, order good food from  <a href='https://play.google.com/store/apps/details?id=com.application.zomato&hl=en_IN&gl=US' target='_blank'>Zomato</a>. </h1>  <h2 style='font-size: 25px; text-align: center; color:  #cb202d;'> But don't you dare order anything from Swiggy!!!! </h2> <h3 style='font-size: 17px; text-align: center; color:  #cb202d;'> They made @thehappybaloney sad by not commenting under their reel. </h3>", unsafe_allow_html=True)
