70c1af2a460108f308a894eae8bc3ca32f656621
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-08-23 06:14:35 +0000

    Chore: Work in_progress

---
+++ chef.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import os
+import key
+from dotenv import load_dotenv
+load_dotenv()
+from pred import model_prediction
+
+CHEF_API_KEY = os.getenv("CHEF_API_KEY")
+
+# Side Bar
+st.sidebar.title("Explore")
+app_mode = st.sidebar.selectbox("Choose the app mode", ["Home", "About Clueless Chef's Aid", "About the Developer"])
+
+# Main Page
+if app_mode == "Home":
+    st.title("Clueless Chef's Aid")
+    st.header("Welcome to Clueless Chef's Aid!")
+    st.write("Chef baloney is here to help you find recipes based on the ingredients you have at home. Simply upload the ingredients you have and we will provide you with a list of recipes you can make.")
+    st.write("Let's get started!")
+    img = st.file_uploader("Upload your a picture of ingredients here", type=["jpg", "png", "jpeg"])
+    if st.button("Analyze"):
+        if img:
+            st.image(img, caption="Uploaded Image", use_column_width=True)
+            st.write("Image uploaded successfully!")
+            st.write("Analyzing image...")
+
+        else:
+            st.write("Please upload an image first!")
+
+    
+    
---
+++ pred.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+
+
+def model_prediction(image):
+    model = tf.keras.models.load_model("trained_model.h5")
+    image = tf.keras.preprocessing.image.load_img(image, target_size=[64,64])
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])
+    
+
