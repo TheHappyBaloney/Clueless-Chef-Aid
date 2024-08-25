26ab124c80e27bb014d357d02fae3d893afdd752
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-08-25 05:28:45 +0000

    feat: Add prediction functionality to model_prediction function

--- pred.py
+++ pred.py
@@ -7,5 +7,7 @@ def model_prediction(image):
     image = tf.keras.preprocessing.image.load_img(image, target_size=[64,64])
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
-    
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions[0])
+
 
